a4e05abefed2553c2b4eac1b4c5d013851a37e60
Ahmed <ahmedkhan895.ak@gmail.com>
2023-07-22 13:48:00 +0100

    event listener added for hover effect

--- main.js
+++ main.js
@@ -3,10 +3,7 @@ let temp;
 let userSize = 16; 
 let square = 200 / userSize; //calculating the size of each div on canvas based on user input 
 arrayVariable.length= userSize * userSize; 
-const pix = document.querySelectorAll('.grid')
-
-// const arrayLength = arrayVariable.length;
-
+const pix = document.getElementsByClassName('grid');
 
 
 
@@ -16,7 +13,26 @@ for (i = 0; i < arrayVariable.length; i++) { //created grid based on size the us
   temp.style.height= square+'px';
   temp.style.width= square+'px';//setting dimensions of divs to relate to user input size 
   document.getElementsByClassName('container')[0].appendChild(temp); 
+  
 }
+
+
+
+for ( let j = 0; j<pix.length; j++) {
+    pix[j].addEventListener('mouseenter', function (){
+        pix[j].classList.add('clicked');
+    });
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
